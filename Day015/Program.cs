295b1b67aa8adc99d911eae1de29584f5529342b
hao.wu <hao.wu@aveva.com>
2021-05-16 22:43:37 +1000

    Day 15 added

---
+++ Day015/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Day015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            string selectedStr = "";
+            var rand = new Random();
+            using (StreamReader sr = new StreamReader(args[0]))
+            {
+                while(sr.Peek() >= 0)
+                {
+                    var s = sr.ReadLine();
+                    var r = rand.Next(0, i);
+                    var selected = r == 0;
+                    if (selected)
+                    {
+                        selectedStr = s;
+                    }
+                    i++;
+                }
+            }
+            System.Console.WriteLine(selectedStr);
+        }
+    }
+}
