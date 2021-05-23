a260fecbbe8a07c728c821686d04164ba2937bc4
hao.wu <hao.wu@aveva.com>
2021-05-23 10:28:44 +1000

    Day 17 added in.

---
+++ Day017/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = args[0];
+            str = str.Replace("\\n", "\n");
+            str = str.Replace("\\t", "\t");
+            var maxLen = GetMaxLen(str, 1, 0);
+            System.Console.WriteLine(maxLen);
+        }
+
+        static int GetMaxLen(string str, int layer, int length)
+        {
+            string pattern1 = "\\t";
+            string pattern2 = "(?!\\t)";
+            string pattern = "\\n";
+            for (var i = 0; i < layer; i++)
+            {
+                pattern += pattern1;
+            }
+            pattern += pattern2;
+            var strs = Regex.Split(str, pattern);
+
+            if (strs.Count() == 1)
+            {
+                return length + str.Length;
+            }
+
+            var maxLen = 0;
+            var len = 0;
+            
+            for (var i = 1; i < strs.Count(); i++)
+            {
+                len = GetMaxLen(strs[i], layer+1, strs[0].Length);
+                if (len > maxLen)
+                {
+                    maxLen = len;
+                }
+            }
+            return length + maxLen + 1;
+        }
+
+    }
+}
